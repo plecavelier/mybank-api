7716c9dd249b5f5eb98d8f2cb24931a8a181000b
plecavelier <pierre.lecavelier@cropandco.com>
2018-12-23 10:01:09 +0100

    Generate JWT keys

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+openssl genrsa -out var/jwt/private.pem -aes256 -passout pass:pass_phrase 4096
+openssl rsa -pubout -in var/jwt/private.pem -out var/jwt/public.pem -passin pass:pass_phrase
+
 composer install --prefer-dist --no-progress --no-suggest
 
 php bin/console doctrine:schema:drop --force
